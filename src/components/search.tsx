5b1f71c675665d29d20e5cd448a5584b3dc559bc
Diego Fernandes <diego.schell.f@gmail.com>
2023-07-14 08:39:49 -0300

    chore: increase command icon on search input

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -62,7 +62,7 @@ export function Search() {
       >
         Search videos...
         <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
-          <span className="text-xs">⌘</span>K
+          <span className="text-base">⌘</span>K
         </kbd>
       </Button>
 
