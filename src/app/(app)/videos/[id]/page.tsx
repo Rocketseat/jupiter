11b81598473c7b0b0f28320855f8ed8b21cbcd97
Diego Fernandes <diego.schell.f@gmail.com>
2023-07-17 11:30:53 -0300

    fix: ESLint integration with Next.js

--- src/app/(app)/videos/[id]/page.tsx
+++ src/app/(app)/videos/[id]/page.tsx
@@ -22,6 +22,8 @@ export async function generateMetadata({
   }
 }
 
+export const dynamic = 'force-dynamic'
+
 export default async function VideoPage({ params }: VideoPageProps) {
   const videoId = params.id
 
