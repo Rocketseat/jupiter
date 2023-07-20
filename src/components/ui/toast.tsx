b06f1469496516e5631d509a9dc7638a2cdb583b
Diego Fernandes <diego.schell.f@gmail.com>
2023-07-20 12:24:17 -0300

    chore: Replace cva with tailwind-variants

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import * as ToastPrimitives from '@radix-ui/react-toast'
-import { cva, type VariantProps } from 'class-variance-authority'
+import { tv, type VariantProps } from 'tailwind-variants'
 import { X } from 'lucide-react'
 
 import { cn } from '@/lib/utils'
@@ -22,21 +22,19 @@ const ToastViewport = React.forwardRef<
 ))
 ToastViewport.displayName = ToastPrimitives.Viewport.displayName
 
-const toastVariants = cva(
-  'group pointer-events-auto relative flex w-full items-center justify-between space-x-4 overflow-hidden rounded-md border border-slate-200 p-6 pr-8 shadow-lg transition-all data-[swipe=cancel]:translate-x-0 data-[swipe=end]:translate-x-[var(--radix-toast-swipe-end-x)] data-[swipe=move]:translate-x-[var(--radix-toast-swipe-move-x)] data-[swipe=move]:transition-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[swipe=end]:animate-out data-[state=closed]:fade-out-80 data-[state=closed]:slide-out-to-right-full data-[state=open]:slide-in-from-top-full data-[state=open]:sm:slide-in-from-bottom-full dark:border-slate-800',
-  {
-    variants: {
-      variant: {
-        default: 'border bg-white dark:bg-slate-950',
-        destructive:
-          'destructive group border-red-500 bg-red-500 text-slate-50 dark:border-red-900 dark:bg-red-900 dark:text-red-50',
-      },
-    },
-    defaultVariants: {
-      variant: 'default',
+const toastVariants = tv({
+  base: 'group pointer-events-auto relative flex w-full items-center justify-between space-x-4 overflow-hidden rounded-md border border-slate-200 p-6 pr-8 shadow-lg transition-all data-[swipe=cancel]:translate-x-0 data-[swipe=end]:translate-x-[var(--radix-toast-swipe-end-x)] data-[swipe=move]:translate-x-[var(--radix-toast-swipe-move-x)] data-[swipe=move]:transition-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[swipe=end]:animate-out data-[state=closed]:fade-out-80 data-[state=closed]:slide-out-to-right-full data-[state=open]:slide-in-from-top-full data-[state=open]:sm:slide-in-from-bottom-full dark:border-slate-800',
+  variants: {
+    variant: {
+      default: 'border bg-white dark:bg-slate-950',
+      destructive:
+        'destructive group border-red-500 bg-red-500 text-slate-50 dark:border-red-900 dark:bg-red-900 dark:text-red-50',
     },
   },
-)
+  defaultVariants: {
+    variant: 'default',
+  },
+})
 
 const Toast = React.forwardRef<
   React.ElementRef<typeof ToastPrimitives.Root>,
