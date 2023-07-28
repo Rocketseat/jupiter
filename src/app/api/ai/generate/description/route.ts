f3138939bbbd8e450b299ccd1b9b2edb27768624
Diego Fernandes <diego.schell.f@gmail.com>
2023-07-28 17:05:06 -0300

    chore: Use GPT-3.5-turbo-16k to generate video description

--- src/app/api/ai/generate/description/route.ts
+++ src/app/api/ai/generate/description/route.ts
@@ -24,7 +24,7 @@ export async function POST(request: Request) {
   }
 
   const response = await openai.createChatCompletion({
-    model: 'gpt-4',
+    model: 'gpt-3.5-turbo-16k',
     stream: true,
     messages: [
       {
