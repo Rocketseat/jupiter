ea7cdad1ae2e3c286f4afd20ae137ea8f943f2fd
Elias Gabriel <eliasgabrielcf@gmail.com>
2023-09-14 14:41:08 -0300

    fix: delete dialog open for all items

--- src/app/(app)/upload/batches/[id]/batch-video-list.tsx
+++ src/app/(app)/upload/batches/[id]/batch-video-list.tsx
@@ -1,14 +1,6 @@
 'use client'
 
 import { CopyButton } from '@/components/copy-button'
-import { Button } from '@/components/ui/button'
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu'
 import {
   Table,
   TableBody,
@@ -18,9 +10,9 @@ import {
   TableRow,
 } from '@/components/ui/table'
 import { formatSecondsToMinutes } from '@/utils/format-seconds-to-minutes'
-import { SymbolIcon, DotsHorizontalIcon } from '@radix-ui/react-icons'
-import { Video, Music2, CopyIcon, Link2, Loader2, Trash2 } from 'lucide-react'
-import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+import { SymbolIcon } from '@radix-ui/react-icons'
+import { CopyIcon, Loader2 } from 'lucide-react'
+import { useQuery } from '@tanstack/react-query'
 import axios from 'axios'
 import { BatchVideoSkeletonTable } from './batch-video-skeleton-table'
 import dayjs from 'dayjs'
@@ -28,19 +20,7 @@ import relativeTime from 'dayjs/plugin/relativeTime'
 import { TranscriptionPreview } from '@/components/transcription-preview'
 import { formatBytes } from '@/utils/format-bytes'
 import Link from 'next/link'
-import {
-  AlertDialog,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-  AlertDialogTrigger,
-} from '@/components/ui/alert-dialog'
-import { useState } from 'react'
-import { ToastAction } from '@/components/ui/toast'
-import { useToast } from '@/components/ui/use-toast'
+import { BatchVideoListActions } from './batch-video-list-actions'
 
 dayjs.extend(relativeTime)
 
@@ -49,11 +29,6 @@ export interface BatchVideoListProps {
 }
 
 export function BatchVideoList({ batchId }: BatchVideoListProps) {
-  const [isDeleteDialogOpen, setIsDeleteDialogOpen] = useState(false)
-
-  const { toast } = useToast()
-  const queryClient = useQueryClient()
-
   const {
     data: batch,
     isLoading: isLoadingBatch,
@@ -69,41 +44,6 @@ export function BatchVideoList({ batchId }: BatchVideoListProps) {
     refetchIntervalInBackground: false,
     refetchOnWindowFocus: true,
   })
-
-  const { isLoading: isDeletingVideo, mutateAsync: deleteVideo } = useMutation(
-    async (videoId: string) => {
-      await axios.delete(`/api/videos/${videoId}`)
-    },
-    {
-      onSuccess() {
-        queryClient.invalidateQueries(['batch', batchId])
-      },
-    },
-  )
-
-  async function handleDeleteVideo(videoId: string) {
-    try {
-      await deleteVideo(videoId)
-
-      setIsDeleteDialogOpen(false)
-    } catch {
-      toast({
-        title: 'Uh oh! Something went wrong.',
-        description:
-          'An error ocurred while trying to delete the video. If the error persists, please contact an administrator.',
-        variant: 'destructive',
-        action: (
-          <ToastAction
-            altText="Try again"
-            disabled={isDeletingVideo}
-            onClick={() => handleDeleteVideo(videoId)}
-          >
-            Try again
-          </ToastAction>
-        ),
-      })
-    }
-  }
 
   return (
     <>
@@ -134,174 +74,69 @@ export function BatchVideoList({ batchId }: BatchVideoListProps) {
             <TableBody>
               {batch.videos.length ? (
                 batch.videos.map((video: any) => (
-                  <AlertDialog
-                    key={video.id}
-                    open={isDeleteDialogOpen}
-                    onOpenChange={setIsDeleteDialogOpen}
-                  >
-                    <TableRow>
-                      <TableCell className="text-center">
-                        {video.uploadOrder}
-                      </TableCell>
-                      <TableCell>
-                        <div className="flex flex-col">
-                          <span className="font-medium"></span>
-                          <Link
-                            href={`/videos/${video.id}`}
-                            prefetch={false}
-                            className="text-violet-500 hover:underline dark:text-violet-300"
-                          >
-                            {video.title}
-                          </Link>
-                          <span className="text-xs text-muted-foreground">
-                            {video.id}
+                  <TableRow key={video.id}>
+                    <TableCell className="text-center">
+                      {video.uploadOrder}
+                    </TableCell>
+                    <TableCell>
+                      <div className="flex flex-col">
+                        <span className="font-medium"></span>
+                        <Link
+                          href={`/videos/${video.id}`}
+                          prefetch={false}
+                          className="text-violet-500 hover:underline dark:text-violet-300"
+                        >
+                          {video.title}
+                        </Link>
+                        <span className="text-xs text-muted-foreground">
+                          {video.id}
+                        </span>
+                      </div>
+                    </TableCell>
+                    <TableCell>
+                      {formatSecondsToMinutes(video.duration)}
+                    </TableCell>
+                    <TableCell>{formatBytes(video.sizeInBytes)}</TableCell>
+                    <TableCell>
+                      {video.transcription ? (
+                        <TranscriptionPreview videoId={video.id} />
+                      ) : (
+                        <div className="flex items-center font-medium">
+                          <SymbolIcon className="mr-2 h-3 w-3 animate-spin" />
+                          <span className="text-muted-foreground">
+                            Processing
                           </span>
                         </div>
-                      </TableCell>
-                      <TableCell>
-                        {formatSecondsToMinutes(video.duration)}
-                      </TableCell>
-                      <TableCell>{formatBytes(video.sizeInBytes)}</TableCell>
-                      <TableCell>
-                        {video.transcription ? (
-                          <TranscriptionPreview videoId={video.id} />
-                        ) : (
-                          <div className="flex items-center font-medium">
-                            <SymbolIcon className="mr-2 h-3 w-3 animate-spin" />
-                            <span className="text-muted-foreground">
-                              Processing
-                            </span>
-                          </div>
-                        )}
-                      </TableCell>
-                      <TableCell>
-                        {video.externalProviderId ? (
-                          <div className="flex items-center gap-2 font-medium">
-                            <span className="truncate text-xs text-muted-foreground">
-                              {video.externalProviderId}
-                            </span>
-                            <CopyButton
-                              size="xs"
-                              variant="outline"
-                              textToCopy={video.externalProviderId}
-                            >
-                              <CopyIcon className="mr-1 h-3 w-3" />
-                              Copy
-                            </CopyButton>
-                          </div>
-                        ) : (
-                          <div className="flex items-center font-medium">
-                            <SymbolIcon className="mr-2 h-3 w-3 animate-spin" />
-                            <span className="text-muted-foreground">
-                              Processing
-                            </span>
-                          </div>
-                        )}
-                      </TableCell>
-                      <TableCell>
-                        <DropdownMenu>
-                          <DropdownMenuTrigger asChild>
-                            <Button
-                              variant="ghost"
-                              className="flex h-8 w-8 p-0 data-[state=open]:bg-muted"
-                            >
-                              <DotsHorizontalIcon className="h-4 w-4" />
-                              <span className="sr-only">Open menu</span>
-                            </Button>
-                          </DropdownMenuTrigger>
-                          <DropdownMenuContent
-                            align="end"
-                            className="w-[160px]"
+                      )}
+                    </TableCell>
+                    <TableCell>
+                      {video.externalProviderId ? (
+                        <div className="flex items-center gap-2 font-medium">
+                          <span className="truncate text-xs text-muted-foreground">
+                            {video.externalProviderId}
+                          </span>
+                          <CopyButton
+                            size="xs"
+                            variant="outline"
+                            textToCopy={video.externalProviderId}
                           >
-                            <DropdownMenuItem
-                              disabled={!video.externalProviderId}
-                              onClick={() =>
-                                navigator.clipboard.writeText(
-                                  `https://b-vz-762f4670-e04.tv.pandavideo.com.br/${video.externalProviderId}/playlist.m3u8`,
-                                )
-                              }
-                            >
-                              <Link2 className="mr-2 h-4 w-4" />
-                              <span>Copy HLS</span>
-                            </DropdownMenuItem>
-                            <DropdownMenuItem asChild>
-                              <a
-                                href={`/api/videos/${video.id}/download/video`}
-                                target="_blank"
-                                rel="noreferrer"
-                              >
-                                <Video className="mr-2 h-4 w-4" />
-                                <span>Download MP4</span>
-                              </a>
-                            </DropdownMenuItem>
-                            <DropdownMenuItem asChild>
-                              <a
-                                href={`/api/videos/${video.id}/download/audio`}
-                                target="_blank"
-                                rel="noreferrer"
-                              >
-                                <Music2 className="mr-2 h-4 w-4" />
-                                <span>Download MP3</span>
-                              </a>
-                            </DropdownMenuItem>
-
-                            <DropdownMenuSeparator />
-
-                            <AlertDialogTrigger asChild>
-                              <DropdownMenuItem disabled={isDeletingVideo}>
-                                <Trash2 className="mr-2 h-4 w-4" />
-                                Delete
-                              </DropdownMenuItem>
-                            </AlertDialogTrigger>
-                          </DropdownMenuContent>
-                        </DropdownMenu>
-
-                        <AlertDialogContent>
-                          <AlertDialogHeader>
-                            <AlertDialogTitle>Are you sure?</AlertDialogTitle>
-                            <AlertDialogDescription className="space-y-3">
-                              <p>
-                                This action can&apos;t be undone and the video
-                                will be permanently deleted from the server.
-                              </p>
-                              <p>
-                                This will{' '}
-                                <span className="font-semibold text-accent-foreground">
-                                  permanently
-                                </span>
-                                :
-                              </p>
-                              <ol className="list-disc space-y-2 pl-4">
-                                <li>
-                                  Delete the MP4, MP3 and subtitles from
-                                  storage;
-                                </li>
-                                <li>Delete the video on external provider;</li>
-                                <li>
-                                  Delete the videos on any outside integration;
-                                </li>
-                              </ol>
-                            </AlertDialogDescription>
-                          </AlertDialogHeader>
-                          <AlertDialogFooter>
-                            <AlertDialogCancel>Cancel</AlertDialogCancel>
-                            <Button
-                              disabled={isDeletingVideo}
-                              variant="destructive"
-                              className="w-20"
-                              onClick={() => handleDeleteVideo(video.id)}
-                            >
-                              {isDeletingVideo ? (
-                                <Loader2 className="h-4 w-4 animate-spin" />
-                              ) : (
-                                'Delete'
-                              )}
-                            </Button>
-                          </AlertDialogFooter>
-                        </AlertDialogContent>
-                      </TableCell>
-                    </TableRow>
-                  </AlertDialog>
+                            <CopyIcon className="mr-1 h-3 w-3" />
+                            Copy
+                          </CopyButton>
+                        </div>
+                      ) : (
+                        <div className="flex items-center font-medium">
+                          <SymbolIcon className="mr-2 h-3 w-3 animate-spin" />
+                          <span className="text-muted-foreground">
+                            Processing
+                          </span>
+                        </div>
+                      )}
+                    </TableCell>
+                    <TableCell>
+                      <BatchVideoListActions batchId={batchId} video={video} />
+                    </TableCell>
+                  </TableRow>
                 ))
               ) : (
                 <TableRow>
